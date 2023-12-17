fbb276d9da3885dc8453ad2862eb233211be6d1a
Barshest113 <barshest113@themailbox.io>
2023-12-17 23:23:31 +0000

    Initial commit + initial page

---
+++ src/_includes/base.php
@@ -0,0 +1,31 @@
+---
+content_placeholder: {{ content }}
+---
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Barshest113</title>
+
+        <link rel="stylesheet" type="text/css" href="/assets/w3css/4.15/w3.css">
+    </head>
+
+    <body>
+        <header class="w3-padding-64 w3-red">
+            <div class="w3-padding-64 w3-center">
+                <p><img src="/assets/images/avatar.jpg" class="w3-iamge w3-circle"></p>
+                <h1><b>Barshest113</b></h1>
+            </div>
+        </header>
+
+        <nav class="w3-light-gray">
+            <div class="w3-auto w3-row">
+                <a href="/" class="w3-button w3-quarter w3-padding-large">Home</a>
+            </div>
+        </nav>
+
+        {{ content }}
+    </body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,11 @@
+---
+layout: src/_includes/base.php
+---
+
+<section>
+    <div class="w3-auto w3-padding">
+        <h2>Welcome to Barshest113!</h2>
+
+        <p>Hello, I am a gamer from Scotland. This is my website, and it is a work in progress. Thank you for visiting!</p>
+    </div>
+</section>
